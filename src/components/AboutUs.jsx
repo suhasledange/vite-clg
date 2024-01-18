46d3d5095ad9c416c1aa0c03eb498191bd63a55f
Suhas Ledange <103985325+suhasledange@users.noreply.github.com>
2024-01-18 18:23:51 +0530

    Update AboutUs.jsx

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,7 +5,10 @@ const AboutUs = () => {
     <div className='lg:flex-1 w-full '>
        
     <div className=' space-y-5 border border-gray-300 bg-white p-5 shadow'>
-    About
+  <h1 className="text-lg mb-4">Introduction : Amrutvahini College of Engineering (AVCOE)
+  </h1>
+      <p className="text-sm">Established in 1983, under the leadership of Hon. Late Bhausaheb Thorat, the Institute has been providing a distinctive excellence driven engineering education that grooms the future generations of engineers and entrepreneurs. Currently the college offers undergraduate and postgraduate courses with annual intake of 840 and 84 respectively.College also provides PhD research center in Mechanical and Civil Engineering. The Institution has the coveted honour of accreditation in 2002 ,2007 and 2012 from NBA, an AICTE wing for quality assurance,New Delhi.Continuing the trend of quaIity education,the institute has re-accredited by NBA for the forth time to all eligible disciplines by maintaining their quality education and research in 2023. The institute has International Academic Accreditation lSO 9001:2015 by TUV SUD in 2017-18. The institute has been Accredited with CGPA of 3.4 on seven point scale at A+ Grade valid for a period of 5 years from 22/11/2021. The college has progressed in leaps and bounds and is continuously improving in all aspects of curricular and extracurricular activities.
+    </p>
     </div>
     </div>
   )
